ca2221d84c5ed93dc7ede10d37d9ab2339bf6823
todd <todd>
2016-04-12 13:33:09 -0700

    Broke the shadow boxes out of the css to move to functions for conditional call

--- functions.php
+++ functions.php
@@ -57,7 +57,39 @@ function ds_plugin_style(){
 	}
 		
 }
-function wptuts_styles_with_the_lot()
+
+function ds_light_theme_shadows(){
+	
+	if(get_option('ds_plugin_style') != 'light-style'){
+		
+		return;
+		
+	}
+	
+	echo '
+		<style>
+		/* Box shadows for light theme. Comment these to remove on grid */
+		.light-theme-shadow .gridder-show{
+		    -webkit-box-shadow: 0px 0px 10px 0px rgba(0,0,0,1);
+ 		   -moz-box-shadow: 0px 0px 10px 0px rgba(0,0,0,1);
+		    box-shadow: 0px 0px 10px 0px rgba(0,0,0,1);
+
+		}
+
+		.light-theme-shadow .gridder-list img{
+ 		   -webkit-box-shadow: 0px 0px 15px 1px rgba(0,0,0,0.75);
+ 		   -moz-box-shadow: 0px 0px 15px 1px rgba(0,0,0,0.75);
+ 		   box-shadow: 0px 0px 15px 1px rgba(0,0,0,0.75);
+		}
+		</style>
+	';
+	
+}
+
+add_action("wp_head", "ds_light_theme_shadows", 990);
+
+
+function ds_styles()
 {
 	  // Register the style like this for a plugin:
 	  wp_register_style( 'ds-style', plugins_url( '/style.css', __FILE__ ), array(), '20120208', 'all' );
@@ -82,7 +114,9 @@ function wptuts_styles_with_the_lot()
 	  // For either a plugin or a theme, you can then enqueue the style:
 	  wp_enqueue_style( 'grid-style' );
 }
-add_action( 'wp_enqueue_scripts', 'wptuts_styles_with_the_lot' );
+add_action( 'wp_enqueue_scripts', 'ds_styles' );
+
+
 function add_my_var($public_query_vars) {
 	  $public_query_vars[] = 'video';
 	$public_query_vars[] = 'channel_category';
