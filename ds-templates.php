c76efe18e8b16de0636c472fdea3aeb6a9a2fb15
Matt Armstrong <matt@stoicbuddha.com>
2016-04-18 13:09:52 -0700

    Fixed template switching issue

--- ds-templates.php
+++ ds-templates.php
@@ -16,7 +16,7 @@ function ds_all_categories_template($single_template) {
 			
 		} else {
 
-			$single_template = get_stylesheet_directory() . '/ds-all-categories.php';
+			$single_template = get_stylesheet_directory() . '/ds-all-categories.tpl.php';
 			
 		} 
 		
@@ -48,7 +48,7 @@ function ds_get_category_template($single_template) {
 			
 		} else {
 
-			$single_template = get_stylesheet_directory() . '/ds-single-category.php';
+			$single_template = get_stylesheet_directory() . '/ds-single-category.tpl.php';
 			
 		} 
 		
@@ -73,10 +73,10 @@ function ds_get_channel_template($single_template) {
 	
      if ($post->post_parent == $channel_parent || $channel_grandparent == $channel_parent) {
 	
-			$template_option = get_option($template_option);
+			$template_option = get_option('ds_channel_template');
 	
 			// Set the template... 
-			if(!locate_template( $template_option . '.tpl.php )){
+			if(!locate_template( $template_option . '.tpl.php ')){
 			
 			// If we can't locate a file named ds-single-category.php that should be the template file, we use our own template...
 						
@@ -85,13 +85,12 @@ function ds_get_channel_template($single_template) {
 				$template_option = "ds-single-channel";
 				
 			}
-			
 				
-				$single_template = dirname( __FILE__ ) . '/templates/single_channel_templates/' . $template_option . '.tpl.php';
+			$single_template = dirname( __FILE__ ) . '/templates/single_channel_templates/' . $template_option . '.tpl.php';
 					
 		} else {
 
-			$single_template = get_stylesheet_directory() . '/ds-single-channel.tpl.php';
+			$single_template = get_stylesheet_directory() . '/' . $template_option . '.tpl.php ';
 			
 		} 
 		
