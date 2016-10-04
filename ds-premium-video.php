25c18d40df78d9ca3e9e4d381734e1c67ead55aa
Matthew Armstrong <matthewarmstrong@Melanies-MacBook-Pro.local>
2016-10-04 11:08:10 -0700

    Fixed videos not having company id on non-child headliners

--- ds-premium-video.php
+++ ds-premium-video.php
@@ -2,10 +2,10 @@
 
 /* 
 ** Plugin Name: dotstudioPRO Premium Video
-** Version: 1.48
+** Version: 1.50
 ** Author: dotstudioPRO
 ** Author URI: # 
-*/
+*/ 
 
 require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
