e2e24b720ce753427014b82a7152ace87b176a8b
Scotty Compton <techguru4hire@gmail.com>
2017-01-20 10:49:18 -0800

    minor tweak to video styling

--- templates/components/channel-video.php
+++ templates/components/channel-video.php
@@ -58,7 +58,7 @@
 			    <div class='row'>
 			    		<div class='col-md-8 ds-video'>
 									<div class='ds-video-fluidMedia'>
-											<div class='ds-player-togglemode'><i class='fa fa-arrows-alt fa-2x'>&nbsp;</i></div>
+											<?php if($recPlaylist === '1'): ?><div class='ds-player-togglemode'><i class='fa fa-arrows-alt fa-2x'>&nbsp;</i></div><?php endif;?>
 											<div class="player" data-minifyVid='<?php echo $minifyVid;?>' data-autoRedir='<?php echo $autoRedir;?>' data-autoPlay='<?php echo $autoPlay;?>' data-recPlaylist='<?php echo $recPlaylist ?>'></div>
 											<script src="<?php echo $headline_video->player ?>"></script>
 									</div>
