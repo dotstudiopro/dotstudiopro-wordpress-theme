d6a5f7502e5fcb52261f1929229941e4470ca58d
Matt Luckey <mattluckey@dotstudiopro.com>
2018-12-03 14:49:33 -0800

    main blog page updates

--- page-templates/main-blog.php
+++ page-templates/main-blog.php
@@ -8,6 +8,7 @@ get_header();
 <div class="custom-container container blog-container">
 	<div class="row">
 		<div class="col-md-8 col-sm-12">
+			<div class="col-md-6 col-sm-12">
 			<?php
 				$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 				// Enforce 15 posts per page; will eventually make this a Theme Option choice
@@ -20,9 +21,10 @@ get_header();
 				<?php while ( have_posts() ) : the_post();
 		    	$post_date = strtotime(get_the_date());
 				?>
-
+				<div class='blog-image'><img class='img img-responsive' src='<?php the_post_thumbnail_url('large'); ?>' /></div>
+			</div>
+			<div class="col-md-6 col-sm-12">
 				<div class='blog-post'>
-		    	<div class='blog-image'><img class='img img-responsive' src='<?php the_post_thumbnail_url('large'); ?>' /></div>
 		    	<div class='blog-title'><?php the_title() ; ?></div>
 		    	<div class='blog-author'><span class='blog-by'>By</span> <?php the_author_meta('display_name'); ?></div>
 		    	<div class='blog-date'>
@@ -32,6 +34,7 @@ get_header();
 		    	<div class='blog-excerpt'>
 		    		<?php the_excerpt(); ?>
 		    	</div>
+		    </div>
 		    </div>
 
 				<?php endwhile; ?>
