89cb33877a6417223f959cfa93cf2bacc9106982
Matt Luckey <mattluckey@dotstudiopro.com>
2018-12-03 16:22:39 -0800

    single page modification

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div class="container">
-    <div class="col-sm-8">
+    <div class="col-md-12 col-sm-12">
         <?php
         if (have_posts()) {
             while (have_posts()) : the_post();
