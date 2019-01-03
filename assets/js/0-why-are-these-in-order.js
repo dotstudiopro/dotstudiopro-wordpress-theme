fc859a829ead51cbab0a21adb990157fa5c10570
Matt <matt@stoicbuddha.com>
2019-01-02 16:43:56 -0800

    Uglified the crap out of the JS; decent drop in DOMContentLoaded ensued

---
+++ assets/js/0-why-are-these-in-order.js
@@ -0,0 +1,5 @@
+/*
+
+	These files are in the order they are in (the ones with number-) because they need to be uglified/concat'd/etc before the other files due to dependencies, and we use a process to uglify/concatenate our JS files that reads the folder contents in order alphabetically. The files that don't have any kind of numerical designation do not have dependencies that require reordering.
+
+*/
