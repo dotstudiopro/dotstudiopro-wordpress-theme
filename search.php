7da6c261432b2fe961e0cbfe7ae104e3180b6f08
Chirag Rupapara <chirag.rupapara@techseria.com>
2023-03-20 16:54:24 +0530

    Add the total parameter on result

--- search.php
+++ search.php
@@ -49,8 +49,8 @@ get_header();
             <nav class="navigation pagination" role="navigation">
                 <div class="nav-links">
                     <?php
-                    if(isset($result['data']['total']) && !empty($result['data']['total'])){
-                        $total_pages = ceil($result['data']['total'] / $dsp_theme_options['opt-search-page-size']);
+                    if(isset($result['total']) && !empty($result['total'])){
+                        $total_pages = ceil($result['total'] / $dsp_theme_options['opt-search-page-size']);
                         if ($total_pages) {
                             $paginate_links = paginate_links(array(
                                 'base' => @add_query_arg('page', '%#%'),
